87605859a6244578ad36e5456100121ec14f6025
Tanawat chinprapinporn <tanawatz@hotmail.com>
2018-12-11 16:54:56 +0700

    Completed

---
+++ workshop501/workshop501/Form1.Designer.vb
@@ -0,0 +1,169 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtId = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.ChkCourse = New System.Windows.Forms.CheckedListBox()
+        Me.ChkEnroll = New System.Windows.Forms.CheckedListBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnRemove = New System.Windows.Forms.Button()
+        Me.btnEnroll = New System.Windows.Forms.Button()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.lblTotalPrice = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(43, 46)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(89, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "เลขบัตรประชาชน"
+        '
+        'txtId
+        '
+        Me.txtId.Location = New System.Drawing.Point(138, 43)
+        Me.txtId.MaxLength = 13
+        Me.txtId.Name = "txtId"
+        Me.txtId.Size = New System.Drawing.Size(251, 20)
+        Me.txtId.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(46, 97)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(61, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "คอร์สเรียน:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(266, 97)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(66, 13)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "คอร์สที่เลือก:"
+        '
+        'ChkCourse
+        '
+        Me.ChkCourse.FormattingEnabled = True
+        Me.ChkCourse.Items.AddRange(New Object() {"English for speaking", "English for reading", "English for writing", "English for listening", "Grammar Course", "Vocabulary Course"})
+        Me.ChkCourse.Location = New System.Drawing.Point(46, 113)
+        Me.ChkCourse.Name = "ChkCourse"
+        Me.ChkCourse.Size = New System.Drawing.Size(120, 124)
+        Me.ChkCourse.TabIndex = 4
+        '
+        'ChkEnroll
+        '
+        Me.ChkEnroll.FormattingEnabled = True
+        Me.ChkEnroll.Location = New System.Drawing.Point(269, 113)
+        Me.ChkEnroll.Name = "ChkEnroll"
+        Me.ChkEnroll.Size = New System.Drawing.Size(120, 124)
+        Me.ChkEnroll.TabIndex = 5
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(179, 131)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(75, 23)
+        Me.btnAdd.TabIndex = 6
+        Me.btnAdd.Text = ">>"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnRemove
+        '
+        Me.btnRemove.Location = New System.Drawing.Point(179, 175)
+        Me.btnRemove.Name = "btnRemove"
+        Me.btnRemove.Size = New System.Drawing.Size(75, 23)
+        Me.btnRemove.TabIndex = 7
+        Me.btnRemove.Text = "<<"
+        Me.btnRemove.UseVisualStyleBackColor = True
+        '
+        'btnEnroll
+        '
+        Me.btnEnroll.Location = New System.Drawing.Point(46, 258)
+        Me.btnEnroll.Name = "btnEnroll"
+        Me.btnEnroll.Size = New System.Drawing.Size(120, 32)
+        Me.btnEnroll.TabIndex = 8
+        Me.btnEnroll.Text = "ลงทะเบียนเรียน"
+        Me.btnEnroll.UseVisualStyleBackColor = True
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Location = New System.Drawing.Point(269, 258)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(120, 32)
+        Me.btnCancel.TabIndex = 9
+        Me.btnCancel.Text = "ยกเลิกการลงทะเบียน"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'lblTotalPrice
+        '
+        Me.lblTotalPrice.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTotalPrice.Location = New System.Drawing.Point(46, 305)
+        Me.lblTotalPrice.Name = "lblTotalPrice"
+        Me.lblTotalPrice.Size = New System.Drawing.Size(343, 30)
+        Me.lblTotalPrice.TabIndex = 10
+        Me.lblTotalPrice.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(437, 344)
+        Me.Controls.Add(Me.lblTotalPrice)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.btnEnroll)
+        Me.Controls.Add(Me.btnRemove)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.ChkEnroll)
+        Me.Controls.Add(Me.ChkCourse)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtId)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "English Course"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtId As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents ChkCourse As CheckedListBox
+    Friend WithEvents ChkEnroll As CheckedListBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnRemove As Button
+    Friend WithEvents btnEnroll As Button
+    Friend WithEvents btnCancel As Button
+    Friend WithEvents lblTotalPrice As Label
+End Class
---
+++ workshop501/workshop501/Form1.vb
@@ -0,0 +1,78 @@
+﻿Public Class Form1
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        Dim countItem As Integer = ChkCourse.Items.Count - 1
+        If ChkCourse.CheckedItems.Count < 1 Then
+            MessageBox.Show("กรุณาเลือกคอร์สเรียน", "WARNING", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Exit Sub
+        End If
+
+        For i = countItem To 0 Step -1
+            If ChkCourse.GetItemChecked(i) Then
+                ChkEnroll.Items.Add(ChkCourse.Items(i))
+                ChkCourse.Items.RemoveAt(i)
+            End If
+        Next
+    End Sub
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim Course() As String = {"English for speaking", "English for reading", "English for writing",
+                                  "English for listening", "Grammar Course", "Vocabulary Course"}
+        ChkEnroll.Items.Clear()
+        txtId.Clear()
+        ChkCourse.Items.Clear()
+        ChkCourse.Items.AddRange(Course)
+        lblTotalPrice.Text = "0.00 Baht"
+    End Sub
+
+    Private Sub btnRemove_Click(sender As Object, e As EventArgs) Handles btnRemove.Click
+        Dim countItem As Integer = ChkEnroll.Items.Count - 1
+        If ChkEnroll.CheckedItems.Count < 1 Then
+            MessageBox.Show("กรุณาเลือกคอร์สเรียน", "WARNING", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Exit Sub
+        End If
+
+        For i = countItem To 0 Step -1
+            If ChkEnroll.GetItemChecked(i) Then
+                ChkCourse.Items.Add(ChkEnroll.Items(i))
+                ChkEnroll.Items.RemoveAt(i)
+            End If
+        Next
+    End Sub
+
+    Private Sub txtId_TextChanged(sender As Object, e As EventArgs) Handles txtId.TextChanged
+        If txtId.TextLength < 13 Then
+            txtId.BackColor = Color.Red
+        Else
+            For i = 0 To txtId.TextLength() - 1 Step 1
+                If IsNumeric(txtId.Text(i)) = False Then
+                    txtId.BackColor = Color.Red
+                    Exit Sub
+                End If
+            Next
+            txtId.BackColor = DefaultBackColor
+        End If
+
+    End Sub
+
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+        Dim course() As String = {"English for speaking", "English for reading", "English for writing", "English for listening",
+                                  "Grammar Course", "Vocabulary Course"}
+        ChkEnroll.Items.Clear()
+        txtId.Clear()
+        txtId.BackColor = DefaultBackColor
+        lblTotalPrice.Text = "0.00 Baht"
+        ChkCourse.Items.Clear()
+        ChkCourse.Items.AddRange(course)
+    End Sub
+
+    Private Sub btnEnroll_Click(sender As Object, e As EventArgs) Handles btnEnroll.Click
+        Dim countItem As Integer = ChkEnroll.Items.Count
+        Dim price As Double = 2500 * countItem
+        If txtId.TextLength > 0 And txtId.BackColor = DefaultBackColor And ChkEnroll.Items.Count > 0 Then
+            lblTotalPrice.Text = String.Format("{0:n}", ChkEnroll.Items.Count * 2500) & " Baht."
+        Else
+            MessageBox.Show("กรุณาเลือกคอร์สเรียน และ/หรือ ป้อนเลขบัตรประชาชนให้ถูกต้อง", "WARNING", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        End If
+    End Sub
+End Class
